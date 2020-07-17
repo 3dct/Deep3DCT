187574df221242aa895cabd95988c57ccbe64352
Patrick <Patrick.weinberger@fh-wels.at>
2020-07-17 13:57:52 +0200

    loss

---
+++ FocalTverskyLoss.py
@@ -0,0 +1,44 @@
+from keras import backend as K
+
+
+
+#Keras
+ALPHA = 0.5
+BETA = 0.5
+
+def TverskyLoss(targets, inputs, alpha=ALPHA, beta=BETA, smooth=1e-6):
+        
+        #flatten label and prediction tensors
+        inputs = K.flatten(inputs)
+        targets = K.flatten(targets)
+        
+        #True Positives, False Positives & False Negatives
+        TP = K.sum((inputs * targets))
+        FP = K.sum(((1-targets) * inputs))
+        FN = K.sum((targets * (1-inputs)))
+       
+        Tversky = (TP + smooth) / (TP + alpha*FP + beta*FN + smooth)  
+        
+        return 1 - Tversky
+
+#Keras
+ALPHA = 0.3
+BETA = 1-ALPHA
+GAMMA = 1
+
+def FocalTverskyLoss(targets, inputs, alpha=ALPHA, beta=BETA, gamma=GAMMA, smooth=1e-6):
+    
+        #flatten label and prediction tensors
+        inputs = K.flatten(inputs)
+        targets = K.flatten(targets)
+        
+        #True Positives, False Positives & False Negatives
+        TP = K.sum((inputs * targets))
+        FP = K.sum(((1-targets) * inputs))
+        FN = K.sum((targets * (1-inputs)))
+               
+        Tversky = (TP + smooth) / (TP + alpha*FP + beta*FN + smooth)  
+        FocalTversky = K.pow((1 - Tversky), gamma)
+        
+        return FocalTversky
+
