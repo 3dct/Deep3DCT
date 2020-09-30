c32012e7e6e61adbe79ceebd28f6c989abd7af75
Patrick <Patrick.weinberger@fh-wels.at>
2020-09-30 16:24:11 +0200

    Dell 14 training ndtinAerospace paper

--- main.py
+++ main.py
@@ -99,12 +99,30 @@ checkpoint = keras.callbacks.ModelCheckpoint(filepath, monitor='loss', verbose=1
 if(len(sys.argv) >= 5):
     model.load_weights(sys.argv[4])
 
+model.load_weights('fibre.hdf5')
+
+def init_layer(layer):
+    weights = proper_shape0 = layer.get_weights()
+    proper_shape0 = layer.get_weights()[0].shape
+    proper_shape1 = layer.get_weights()[1].shape
+    weights = [ np.random.normal(loc=0.0, scale=0.1, size=proper_shape0),np.random.normal(loc=0.0, scale=0.1, size=proper_shape1)]
+    layer.set_weights(weights)
+
+layer = model.get_layer('conv3d_20')
+init_layer(layer)
+layer = model.get_layer('conv3d_21')
+init_layer(layer)
+layer = model.get_layer('conv3d_22')
+init_layer(layer)
+layer = model.get_layer('conv3d_23')
+init_layer(layer)
+
 #model.fit(x=gen_3D , callbacks=[checkpoint],steps_per_epoch=240, epochs=100)
 
 
 #dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train)).batch(1)
 #dataset_val = tf.data.Dataset.from_tensor_slices((X_test, y_test)).batch(1)
-model.fit(TrainGenerator, validation_data=ValGenerator, steps_per_epoch= len(X), validation_steps=len(ValX), callbacks=[checkpoint], epochs=numEpochs)
+model.fit(TrainGenerator, validation_data=ValGenerator, steps_per_epoch= len(X)/batchSize, validation_steps=len(ValX)/batchSize, callbacks=[checkpoint], epochs=numEpochs)
 
 """ 
 TestResults = model.evaluate(X_test,y_test,batch_size=2)
